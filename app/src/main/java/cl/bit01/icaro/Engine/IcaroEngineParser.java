ccc0dec72b40854973193f493ef976956efe5544
Miguel Angel Zapata <miguelo.zapata@gmail.com>
2015-02-04 22:29:19 -0300

    Added Changes from EngineParser

--- app/src/main/java/cl/bit01/icaro/Engine/IcaroEngineParser.java
+++ app/src/main/java/cl/bit01/icaro/Engine/IcaroEngineParser.java
@@ -1,7 +1,6 @@
 // Generated from /media/HD/Documentos/Proyectos Git/Icaro Engine/IcaroEngine.g4 by ANTLR 4.5
 
 package cl.bit01.icaro.Engine;
-
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.util.Log;
@@ -22,28 +21,37 @@ import org.antlr.v4.runtime.dfa.DFA;
 import org.antlr.v4.runtime.tree.ParseTreeListener;
 import org.antlr.v4.runtime.tree.TerminalNode;
 
-import cl.bit01.icaro.Fragments.Funcionalidades.Hora;
+import cl.bit01.icaro.Fragments.Funcionalidades.Clock;
 import cl.bit01.icaro.R;
 
 @SuppressWarnings({"all", "warnings", "unchecked", "unused", "cast"})
 public class IcaroEngineParser extends Parser {
-    static {
-        RuntimeMetaData.checkVersion("4.5", RuntimeMetaData.VERSION);
-    }
+    static { RuntimeMetaData.checkVersion("4.5", RuntimeMetaData.VERSION); }
 
+    protected static final DFA[] _decisionToDFA;
+    protected static final PredictionContextCache _sharedContextCache =
+            new PredictionContextCache();
     public static final int
-            T__0 = 1, T__1 = 2, PETICION = 3, RELOJ = 4, INTERROGACION = 5;
+            T__0=1, T__1=2, PETICION=3, RELOJ=4, INTERROGACION=5;
     public static final int
             RULE_icaro = 0, RULE_funcion = 1, RULE_reloj = 2;
     public static final String[] ruleNames = {
             "icaro", "funcion", "reloj"
     };
+
+    private static final String[] _LITERAL_NAMES = {
+            null, "'la'", "'es'", null, "'hora'", "'que'"
+    };
+    private static final String[] _SYMBOLIC_NAMES = {
+            null, null, null, "PETICION", "RELOJ", "INTERROGACION"
+    };
+    public static final Vocabulary VOCABULARY = new VocabularyImpl(_LITERAL_NAMES, _SYMBOLIC_NAMES);
+
     /**
      * @deprecated Use {@link #VOCABULARY} instead.
      */
     @Deprecated
     public static final String[] tokenNames;
-
     static {
         tokenNames = new String[_SYMBOLIC_NAMES.length];
         for (int i = 0; i < tokenNames.length; i++) {
@@ -58,39 +66,7 @@ public class IcaroEngineParser extends Parser {
         }
     }
 
-    public static final String _serializedATN =
-            "\3\u0430\ud6d1\u8206\uad2d\u4417\uaef1\u8d80\uaadd\3\7\27\4\2\t\2\4\3" +
-                    "\t\3\4\4\t\4\3\2\3\2\3\3\3\3\3\4\3\4\3\4\3\4\3\4\3\4\5\4\23\n\4\3\4\3" +
-                    "\4\3\4\2\2\5\2\4\6\2\2\24\2\b\3\2\2\2\4\n\3\2\2\2\6\22\3\2\2\2\b\t\5\4" +
-                    "\3\2\t\3\3\2\2\2\n\13\5\6\4\2\13\5\3\2\2\2\f\r\7\5\2\2\r\16\7\3\2\2\16" +
-                    "\23\7\6\2\2\17\20\7\7\2\2\20\21\7\6\2\2\21\23\7\4\2\2\22\f\3\2\2\2\22" +
-                    "\17\3\2\2\2\23\24\3\2\2\2\24\25\b\4\1\2\25\7\3\2\2\2\3\22";
-    public static final ATN _ATN =
-            new ATNDeserializer().deserialize(_serializedATN.toCharArray());
-
-    static {
-        _decisionToDFA = new DFA[_ATN.getNumberOfDecisions()];
-        for (int i = 0; i < _ATN.getNumberOfDecisions(); i++) {
-            _decisionToDFA[i] = new DFA(_ATN.getDecisionState(i), i);
-        }
-    }
-
-    protected static final DFA[] _decisionToDFA;
-    protected static final PredictionContextCache _sharedContextCache =
-            new PredictionContextCache();
-    private static final String[] _LITERAL_NAMES = {
-            null, "'la'", "'es'", null, "'hora'", "'que'"
-    };
-    private static final String[] _SYMBOLIC_NAMES = {
-            null, null, null, "PETICION", "RELOJ", "INTERROGACION"
-    };
-    public static final Vocabulary VOCABULARY = new VocabularyImpl(_LITERAL_NAMES, _SYMBOLIC_NAMES);
     private FragmentManager mFragmentManager;
-
-    public IcaroEngineParser(TokenStream input) {
-        super(input);
-        _interp = new ParserATNSimulator(this, _ATN, _decisionToDFA, _sharedContextCache);
-    }
 
     @Override
     @Deprecated
@@ -105,23 +81,37 @@ public class IcaroEngineParser extends Parser {
     }
 
     @Override
-    public String getGrammarFileName() {
-        return "IcaroEngine.g4";
-    }
+    public String getGrammarFileName() { return "IcaroEngine.g4"; }
 
     @Override
-    public String[] getRuleNames() {
-        return ruleNames;
-    }
+    public String[] getRuleNames() { return ruleNames; }
 
     @Override
-    public String getSerializedATN() {
-        return _serializedATN;
-    }
+    public String getSerializedATN() { return _serializedATN; }
 
     @Override
-    public ATN getATN() {
-        return _ATN;
+    public ATN getATN() { return _ATN; }
+
+    public IcaroEngineParser(TokenStream input) {
+        super(input);
+        _interp = new ParserATNSimulator(this,_ATN,_decisionToDFA,_sharedContextCache);
+    }
+    public static class IcaroContext extends ParserRuleContext {
+        public FuncionContext funcion() {
+            return getRuleContext(FuncionContext.class,0);
+        }
+        public IcaroContext(ParserRuleContext parent, int invokingState) {
+            super(parent, invokingState);
+        }
+        @Override public int getRuleIndex() { return RULE_icaro; }
+        @Override
+        public void enterRule(ParseTreeListener listener) {
+            if ( listener instanceof IcaroEngineListener ) ((IcaroEngineListener)listener).enterIcaro(this);
+        }
+        @Override
+        public void exitRule(ParseTreeListener listener) {
+            if ( listener instanceof IcaroEngineListener ) ((IcaroEngineListener)listener).exitIcaro(this);
+        }
     }
 
     public final IcaroContext icaro(FragmentManager mFragmentManager) throws RecognitionException {
@@ -134,14 +124,34 @@ public class IcaroEngineParser extends Parser {
                 setState(6);
                 funcion();
             }
-        } catch (RecognitionException re) {
+        }
+        catch (RecognitionException re) {
             _localctx.exception = re;
             _errHandler.reportError(this, re);
             _errHandler.recover(this, re);
-        } finally {
+        }
+        finally {
             exitRule();
         }
         return _localctx;
+    }
+
+    public static class FuncionContext extends ParserRuleContext {
+        public RelojContext reloj() {
+            return getRuleContext(RelojContext.class,0);
+        }
+        public FuncionContext(ParserRuleContext parent, int invokingState) {
+            super(parent, invokingState);
+        }
+        @Override public int getRuleIndex() { return RULE_funcion; }
+        @Override
+        public void enterRule(ParseTreeListener listener) {
+            if ( listener instanceof IcaroEngineListener ) ((IcaroEngineListener)listener).enterFuncion(this);
+        }
+        @Override
+        public void exitRule(ParseTreeListener listener) {
+            if ( listener instanceof IcaroEngineListener ) ((IcaroEngineListener)listener).exitFuncion(this);
+        }
     }
 
     public final FuncionContext funcion() throws RecognitionException {
@@ -153,14 +163,34 @@ public class IcaroEngineParser extends Parser {
                 setState(8);
                 reloj();
             }
-        } catch (RecognitionException re) {
+        }
+        catch (RecognitionException re) {
             _localctx.exception = re;
             _errHandler.reportError(this, re);
             _errHandler.recover(this, re);
-        } finally {
+        }
+        finally {
             exitRule();
         }
         return _localctx;
+    }
+
+    public static class RelojContext extends ParserRuleContext {
+        public TerminalNode PETICION() { return getToken(IcaroEngineParser.PETICION, 0); }
+        public TerminalNode RELOJ() { return getToken(IcaroEngineParser.RELOJ, 0); }
+        public TerminalNode INTERROGACION() { return getToken(IcaroEngineParser.INTERROGACION, 0); }
+        public RelojContext(ParserRuleContext parent, int invokingState) {
+            super(parent, invokingState);
+        }
+        @Override public int getRuleIndex() { return RULE_reloj; }
+        @Override
+        public void enterRule(ParseTreeListener listener) {
+            if ( listener instanceof IcaroEngineListener ) ((IcaroEngineListener)listener).enterReloj(this);
+        }
+        @Override
+        public void exitRule(ParseTreeListener listener) {
+            if ( listener instanceof IcaroEngineListener ) ((IcaroEngineListener)listener).exitReloj(this);
+        }
     }
 
     public final RelojContext reloj() throws RecognitionException {
@@ -171,7 +201,8 @@ public class IcaroEngineParser extends Parser {
             {
                 setState(16);
                 switch (_input.LA(1)) {
-                    case PETICION: {
+                    case PETICION:
+                    {
                         {
                             {
                                 setState(10);
@@ -186,7 +217,8 @@ public class IcaroEngineParser extends Parser {
                         }
                     }
                     break;
-                    case INTERROGACION: {
+                    case INTERROGACION:
+                    {
                         {
                             {
                                 setState(13);
@@ -209,107 +241,36 @@ public class IcaroEngineParser extends Parser {
 
                 Log.d("Icaro", "ENGINE - Funcion Hora Reconocida");
                 mFragmentManager.beginTransaction()
-                        .replace(R.id.fragmentMainIcaro, new Hora())
+                        .replace(R.id.fragmentMainIcaro, new Clock())
                         .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
                         .commit();
 
             }
-        } catch (RecognitionException re) {
+        }
+        catch (RecognitionException re) {
             _localctx.exception = re;
             _errHandler.reportError(this, re);
             _errHandler.recover(this, re);
-        } finally {
+        }
+        finally {
             exitRule();
         }
         return _localctx;
     }
 
-    public static class IcaroContext extends ParserRuleContext {
-        public IcaroContext(ParserRuleContext parent, int invokingState) {
-            super(parent, invokingState);
-        }
-
-        public FuncionContext funcion() {
-            return getRuleContext(FuncionContext.class, 0);
-        }
-
-        @Override
-        public int getRuleIndex() {
-            return RULE_icaro;
-        }
-
-        @Override
-        public void enterRule(ParseTreeListener listener) {
-            if (listener instanceof IcaroEngineListener)
-                ((IcaroEngineListener) listener).enterIcaro(this);
-        }
-
-        @Override
-        public void exitRule(ParseTreeListener listener) {
-            if (listener instanceof IcaroEngineListener)
-                ((IcaroEngineListener) listener).exitIcaro(this);
-        }
-    }
-
-    public static class FuncionContext extends ParserRuleContext {
-        public FuncionContext(ParserRuleContext parent, int invokingState) {
-            super(parent, invokingState);
-        }
-
-        public RelojContext reloj() {
-            return getRuleContext(RelojContext.class, 0);
-        }
-
-        @Override
-        public int getRuleIndex() {
-            return RULE_funcion;
-        }
-
-        @Override
-        public void enterRule(ParseTreeListener listener) {
-            if (listener instanceof IcaroEngineListener)
-                ((IcaroEngineListener) listener).enterFuncion(this);
-        }
-
-        @Override
-        public void exitRule(ParseTreeListener listener) {
-            if (listener instanceof IcaroEngineListener)
-                ((IcaroEngineListener) listener).exitFuncion(this);
-        }
-    }
-
-    public static class RelojContext extends ParserRuleContext {
-        public RelojContext(ParserRuleContext parent, int invokingState) {
-            super(parent, invokingState);
-        }
-
-        public TerminalNode PETICION() {
-            return getToken(IcaroEngineParser.PETICION, 0);
-        }
-
-        public TerminalNode RELOJ() {
-            return getToken(IcaroEngineParser.RELOJ, 0);
-        }
-
-        public TerminalNode INTERROGACION() {
-            return getToken(IcaroEngineParser.INTERROGACION, 0);
-        }
-
-        @Override
-        public int getRuleIndex() {
-            return RULE_reloj;
-        }
-
-        @Override
-        public void enterRule(ParseTreeListener listener) {
-            if (listener instanceof IcaroEngineListener)
-                ((IcaroEngineListener) listener).enterReloj(this);
-        }
-
-        @Override
-        public void exitRule(ParseTreeListener listener) {
-            if (listener instanceof IcaroEngineListener)
-                ((IcaroEngineListener) listener).exitReloj(this);
+    public static final String _serializedATN =
+            "\3\u0430\ud6d1\u8206\uad2d\u4417\uaef1\u8d80\uaadd\3\7\27\4\2\t\2\4\3"+
+                    "\t\3\4\4\t\4\3\2\3\2\3\3\3\3\3\4\3\4\3\4\3\4\3\4\3\4\5\4\23\n\4\3\4\3"+
+                    "\4\3\4\2\2\5\2\4\6\2\2\24\2\b\3\2\2\2\4\n\3\2\2\2\6\22\3\2\2\2\b\t\5\4"+
+                    "\3\2\t\3\3\2\2\2\n\13\5\6\4\2\13\5\3\2\2\2\f\r\7\5\2\2\r\16\7\3\2\2\16"+
+                    "\23\7\6\2\2\17\20\7\7\2\2\20\21\7\6\2\2\21\23\7\4\2\2\22\f\3\2\2\2\22"+
+                    "\17\3\2\2\2\23\24\3\2\2\2\24\25\b\4\1\2\25\7\3\2\2\2\3\22";
+    public static final ATN _ATN =
+            new ATNDeserializer().deserialize(_serializedATN.toCharArray());
+    static {
+        _decisionToDFA = new DFA[_ATN.getNumberOfDecisions()];
+        for (int i = 0; i < _ATN.getNumberOfDecisions(); i++) {
+            _decisionToDFA[i] = new DFA(_ATN.getDecisionState(i), i);
         }
     }
 }
