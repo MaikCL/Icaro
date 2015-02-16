c2a2551e91d5e6b6f36e1035f7b4db98687d8926
Miguel Angel Zapata <miguelo.zapata@gmail.com>
2015-02-15 22:33:05 -0300

    Added Initialize elements

--- app/src/main/java/cl/bit01/icaro/Fragments/Features/Business.java
+++ app/src/main/java/cl/bit01/icaro/Fragments/Features/Business.java
@@ -4,6 +4,104 @@ package cl.bit01.icaro.Fragments.Features;
  */
 
 import android.app.Fragment;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+
+import cl.bit01.icaro.R;
+import cl.bit01.icaro.Utils.GPSTracker;
 
 public class Business extends Fragment {
+    private FrameLayout layout;
+    private TextView name;
+    private TextView address;
+    private TextView secondaryAddress;
+    private TextView distance;
+    private TextView phone;
+    private TextView poweredBy;
+    private MapView mMapView;
+    private GoogleMap mGoogleMap;
+    private Bundle mBundle;
+    private GPSTracker gps;
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstance) {
+        View view = inflater.inflate(R.layout.fragment_business, container, false);
+        MapsInitializer.initialize(getActivity());
+        Bundle bundle = this.getArguments();
+
+        Toolbar mToolbarCard = (Toolbar) view.findViewById(R.id.toolbar_business);
+        layout = (FrameLayout) view.findViewById(R.id.business_layout);
+        name = (TextView) view.findViewById(R.id.business_name);
+        address = (TextView) view.findViewById(R.id.business_address);
+        secondaryAddress = (TextView) view.findViewById(R.id.business_secondaryAddress);
+        distance = (TextView) view.findViewById(R.id.business_distance);
+        phone = (TextView) view.findViewById(R.id.business_phone);
+        poweredBy = (TextView) view.findViewById(R.id.business_poweredBy);
+        mToolbarCard.setTitle("Explorando Negocios");
+        layout.setVisibility(View.INVISIBLE);
+
+        mMapView.onCreate(mBundle);
+        initMapView(view);
+
+        if (bundle.getString("mode").equals("explore")) {
+            exploreBusiness(bundle.getString("business"), bundle.getBoolean("near"));
+        } //else (bundle.getString("mode").equals("search")){
+
+        //}
+        return view;
+    }
+
+    private void exploreBusiness(String business, boolean near) {
+    }
+
+    private void setCurrentLocation() {
+
+    }
+
+    private void initMapView(View view) {
+        if (mGoogleMap == null) {
+            mGoogleMap = ((MapView) view.findViewById(R.id.business_googlemap)).getMap();
+            if (mGoogleMap != null) {
+                setUpMap();
+            }
+        }
+    }
+
+    private void setUpMap() {
+        mGoogleMap.setMyLocationEnabled(true);
+        mGoogleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mBundle = savedInstanceState;
+        gps = new GPSTracker(getActivity());
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mMapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mMapView.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        mMapView.onDestroy();
+        super.onDestroy();
+    }
 }
