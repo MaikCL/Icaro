881d012c96577d77ae1710ac6e3a17056b84697b
Miguel A. Zapata <miguel.zapatae@gmail.com>
2015-04-29 17:31:10 -0400

    Added support library for cardviews Added fragment for reciclerview and added specific cardview for news function

--- app/src/main/java/cl/mzapatae/icaro/Features/News.java
+++ app/src/main/java/cl/mzapatae/icaro/Features/News.java
@@ -24,7 +24,7 @@ public class News extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_news, container, false);
+        return inflater.inflate(R.layout.fragment_recicler_view, container, false);
     }
 
 
