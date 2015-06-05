c1813b1226dbc8dda770db46621fddfce7a6a8fe
Miguel A. Zapata <miguel.zapatae@gmail.com>
2015-06-05 18:10:34 -0400

    Added About Me [WIP]

--- app/src/main/java/cl/mzapatae/icaro/Fragments/SettingsFragment.java
+++ app/src/main/java/cl/mzapatae/icaro/Fragments/SettingsFragment.java
@@ -4,15 +4,41 @@ package cl.mzapatae.icaro.Fragments;
  * Created by mzapata on 05-06-2015.
  */
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
 
 import cl.bit01.icaro.R;
+import cl.mzapatae.icaro.Activities.AboutMe;
 
-public class SettingsFragment extends PreferenceFragment {
+public class SettingsFragment extends PreferenceFragment implements Preference.OnPreferenceClickListener {
+    final String TWITTER_LOGIN = "key_loginTwitter";
+    final String ABOUT_ME = "key_aboutMe";
+    final String HELP = "key_help";
+
     @Override
     public void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.settings);
+
+        Preference twitterLogin = findPreference(TWITTER_LOGIN);
+        Preference helpScreen = findPreference(HELP);
+        Preference aboutMe = findPreference(ABOUT_ME);
+
+        twitterLogin.setOnPreferenceClickListener(this);
+        helpScreen.setOnPreferenceClickListener(this);
+        aboutMe.setOnPreferenceClickListener(this);
+    }
+
+    @Override
+    public boolean onPreferenceClick(Preference preference) {
+
+        if (preference.getKey().equals(ABOUT_ME)) {
+            Intent aboutMeScreen = new Intent(this.getActivity(), AboutMe.class);
+            this.getActivity().startActivity(aboutMeScreen);
+        }
+
+        return false;
     }
 }
