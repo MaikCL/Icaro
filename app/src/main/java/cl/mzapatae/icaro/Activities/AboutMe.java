f88e0b2e6b1327c55e44d78c86ca4da93ce85e0a
Miguel A. Zapata <miguel.zapatae@gmail.com>
2015-06-09 18:04:27 -0400

    Changes in About Me Dialog Layout

--- app/src/main/java/cl/mzapatae/icaro/Activities/AboutMe.java
+++ app/src/main/java/cl/mzapatae/icaro/Activities/AboutMe.java
@@ -3,12 +3,17 @@ package cl.mzapatae.icaro.Activities;
 import android.app.Dialog;
 import android.content.Context;
 import android.os.Bundle;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
 import android.view.Window;
+import android.widget.TextView;
 
 import cl.bit01.icaro.R;
 
 public class AboutMe extends Dialog {
     private static Context mContext = null;
+    private TextView twitterText;
+
 
     public AboutMe(Context context) {
         super(context);
@@ -20,6 +25,10 @@ public class AboutMe extends Dialog {
     public void onCreate(Bundle savedInstanceState) {
         this.requestWindowFeature(Window.FEATURE_LEFT_ICON);
         setContentView(R.layout.dialog_aboutme);
+        twitterText = (TextView) findViewById(R.id.about_twitterName);
+        twitterText.setText(Html.fromHtml("<a href=\"" + "https://twitter.com/intent/user?screen_name=mzapatae_" + "\">" + "@MZapatae_" + "</a>"));
+        twitterText.setClickable(true);
+        twitterText.setMovementMethod(LinkMovementMethod.getInstance());
     }
 
 }
